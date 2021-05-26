942e642fbe2ba09efa1fad7ff678711fc085ff34
FabioKrakauer <comvocefabio@gmail.com>
2021-05-26 20:27:23 -0300

    add address service

--- app/address/services.py
+++ app/address/services.py
@@ -1,10 +1,24 @@
+from .models import Address
 import requests
+import os
+import logging
+logger = logging.getLogger(__name__)
 
-class AccountService():
-    def getCEPCoords(cep):
-    url = f"https://www.cepaberto.com/api/v3/cep?cep={cep}"
+class AddressService():
+    def getZIPCoords(cep):
+        cep = AddressService.get_cep_numbers(cep)
+        url = f"https://www.cepaberto.com/api/v3/cep?cep={cep}"
+        headers = {'Authorization': "Token token=35f9cb346092abca1d8251546b77a1cc"}
+        response = requests.get(url, headers=headers)
+        coords = {"latitude": response.json()["latitude"], "longitude": response.json()["longitude"]}
+        return coords
 
-    headers = {'Authorization': 'Token token=35f9cb346092abca1d8251546b77a1cc'}
-    response = requests.get(url, headers=headers)
-    coords = {"latitude", response.json()["latitude"], "longitude": response.json()["longitude"]}
-    return coords
+    def getCoords(address_id):
+        address = Address.objects.get(id=address_id)
+        return AddressService.getZIPCoords(address.zipCode)
+    
+    def get_cep_numbers(cep):
+        cep_formatted = str(cep).replace('-', '')
+        if len(cep_formatted) != 8:
+            cep_formatted = "0" + cep_formatted
+        return cep_formatted
